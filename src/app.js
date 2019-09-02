efede1d9faae6b99aba5273e94a55aa9cc81f50e
Philippe Haerens <haerphi@gmail.com>
2019-09-02 12:41:40 +0200

    Hello world un peu plus avancer

--- src/app.js
+++ src/app.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import helloImg from "./img/hello.jpeg";
 
-const App = () => (
-    <div>
-        <img src={helloImg} />
-        <h1>{"Hello parcel world"}</h1>
-    </div>
-);
+const App = () => {
+    const hello = "Hello parcel world";
+    return (
+        <div>
+            <img src={helloImg} />
+            <h1>{hello}</h1>
+        </div>
+    );
+};
 
 ReactDOM.render(<App />, document.querySelector("#root"));
